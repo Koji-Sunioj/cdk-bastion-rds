91db2670eaa94e5530bf6f0da6e5eb693114e2cb
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-01-30 08:46:16 +0200

    Initial commit

---
+++ test/auroratest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Auroratest from '../lib/auroratest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auroratest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Auroratest.AuroratestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
